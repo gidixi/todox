796ddbb403645f16f2e61e640f3555851ab27d65
gidiesse <g.desolda@gmail.com>
2023-04-05 22:33:20 +0200

    Aggiunta data e ora

--- src/App.js
+++ src/App.js
@@ -4,20 +4,38 @@ import { Button, Card, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-function Todo({ todo, index, markTodo, removeTodo }) {
+function Todo({ todo, index, markTodo, removeTodo, }) {
   return (
-    <div
-      className="todo"
-      
-    >
-      <span style={{ textDecoration: todo.isDone ? "line-through" : "" }}>{todo.text}</span>
-      <div>
-        <Button variant="outline-success" onClick={() => markTodo(index)}>✓</Button>{' '}
-        <Button variant="outline-danger" onClick={() => removeTodo(index)}>✕</Button>
-      </div>
+    <div className="todo">
+      <Card className="cardDimesione">
+        <Card.Header>
+
+          {todo.date && (
+            <small /*className="text-muted cardDimRight"*/ className={todo.isDone ? "completed text-muted cardDimRight" : "text-muted cardDimRight"}>
+              Aggiunto il {new Date(todo.date).toLocaleString()}
+            </small>
+          )}
+        </Card.Header>
+        <Card.Body>
+          <span style={{ textDecoration: todo.isDone ? "line-through" : "" }}>
+            {todo.text}
+          </span>
+          <Button className="btnConfDel" variant="outline-success" onClick={() => markTodo(index)}>
+            ✓
+          </Button>{" "}
+          <Button className="btnConfDel" variant="outline-danger" onClick={() => removeTodo(index)}>
+            ✕
+          </Button>
+          
+          
+        </Card.Body>
+      </Card>
     </div>
   );
 }
+
+
+
 
 function FormTodo({ addTodo }) {
   const [value, setValue] = React.useState("");
@@ -25,28 +43,31 @@ function FormTodo({ addTodo }) {
   const handleSubmit = e => {
     e.preventDefault();
     if (!value) return;
-    addTodo(value);
+    addTodo({
+      text: value,
+      date: Date.now() // aggiunge la data corrente come proprietà date dell'oggetto todo
+    });
     setValue("");
   };
 
-  
 
- 
+
+
 
   return (
-    <Form onSubmit={handleSubmit}> 
-    <Form.Group>
-      <Form.Label><b>Cose da fare</b></Form.Label>
-      <Form.Control type="text" id="input" className="input" value={value} onChange={e => setValue(e.target.value)} placeholder="Aggiungi Todo" />
-    </Form.Group>
-    <Button className="bottone" type="submit" > 
-      Aggiungi
-    </Button>
-  </Form>
+    <Form onSubmit={handleSubmit}>
+      <Form.Group>
+        <Form.Label><b>Cose da fare</b></Form.Label>
+        <Form.Control type="text" id="input" className="input" value={value} onChange={e => setValue(e.target.value)} placeholder="Aggiungi Todo" />
+      </Form.Group>
+      <Button className="bottone" type="submit" >
+        Aggiungi
+      </Button>
+    </Form>
   );
 }
 
-
+/*
 function App() { 
   
 
@@ -104,5 +125,57 @@ function App() {
     </div>
   );
 }
+*/
+
+function App() {
+  const [todos, setTodos] = React.useState(
+    JSON.parse(localStorage.getItem("todos")) || []
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodo = todo => {
+    const newTodos = [...todos, todo];
+    setTodos(newTodos);
+  };
+
+  const markTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isDone = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <div className="container">
+        <h1 className="text-center mb-4">Todo-X</h1>
+        <FormTodo addTodo={addTodo} />
+        <div>
+          {todos.map((todo, index) => (
+            <Card className="code" key={index}>
+              <Card.Body>
+                <Todo
+                  index={index}
+                  todo={todo}
+                  markTodo={markTodo}
+                  removeTodo={removeTodo}
+                />
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
 
 export default App;
